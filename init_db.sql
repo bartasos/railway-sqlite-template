1cb5a215aca386338f528f0caf9f2935b6a227bf
Vojtěch Vavera <vaveravojta@gmail.com>
2023-08-12 12:00:28 +0200

    db init

--- init_db.sql
+++ init_db.sql
@@ -1,2 +1,18 @@
 -- Here you can add any sql you want such as queries to create tables, insert data, etc.
 -- The file will only be executed when you change anything in it and will run only once.
+-- SQLite
+CREATE TABLE IF NOT EXISTS channels_types(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(64) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS channels(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    guild_id INTEGER NOT NULL,
+    guild_name VARCHAR(64) NOT NULL,
+    channel_type INTEGER NOT NULL,
+    channel_id INTEGER NOT NULL,
+    channel_name VARCHAR(64) NOT NULL,
+    CONSTRAINT fk_channels_channel_type 
+        FOREIGN KEY(channel_type) 
+        REFERENCES channels_types(id)
+);
